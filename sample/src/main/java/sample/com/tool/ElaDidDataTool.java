51828d17a2984e487552bc0d223b5588ce4ddcc8
zuohuahua <zuohuahua@elastos.org>
2019-04-17 16:22:14 +0800

    AppID tool add parameter and upload data
    --name set app name
    --mne set mnemonic, if not set, will generate a new one
    
    output add upload data

--- sample/src/main/java/sample/com/tool/ElaDidDataTool.java
+++ sample/src/main/java/sample/com/tool/ElaDidDataTool.java
@@ -5,6 +5,7 @@ import org.elastos.service.ElaDidService;
 import org.elastos.service.ElaDidServiceImp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.elastos.POJO.DidEntity;
 
 import java.util.*;
 
@@ -18,11 +19,20 @@ public class ElaDidDataTool {
     String didPrivateKey = "";
     String didPublicKey = "";
     String did = "";
+    String upload = "";
+
+    final String help = "--name         the app name.\n" +
+            "--mne          mnemonic, if not set, will generate a new one.\n\n";
 
     ElaDidService didService = new ElaDidServiceImp();
 
-    public void createAppInfoAndDid(String inAppName) throws Exception {
-        didMnemonic = didService.createDidMnemonic();
+    public void createAppInfoAndDid(String inAppName, String inMnemonic) throws Exception {
+
+        if (inMnemonic.isEmpty()) {
+            didMnemonic = didService.createDidMnemonic();
+        } else {
+            didMnemonic = inMnemonic;
+        }
 
         String ret = didService.createDidByMnemonic(didMnemonic);
         Map data = JSON.parseObject(ret, Map.class);
@@ -33,6 +43,27 @@ public class ElaDidDataTool {
         appName = inAppName;
         appId = didService.signDidMessage(didPrivateKey, appName);
         addIdSign = didService.signDidMessage(didPrivateKey, appId);
+
+
+        List<DidEntity.DidProperty> properties = new ArrayList<>();
+
+        if (inMnemonic.isEmpty()) {
+            DidEntity.DidProperty pubkeyProperty = new DidEntity.DidProperty();
+            pubkeyProperty.setKey("PublicKey");
+            pubkeyProperty.setValue(didPublicKey);
+            properties.add(pubkeyProperty);
+        }
+
+        DidEntity.DidProperty appIDProperty = new DidEntity.DidProperty();
+        appIDProperty.setKey("Dev/" + appName + "/AppID");
+        appIDProperty.setValue(appId);
+        properties.add(appIDProperty);
+
+        DidEntity didEntity = new DidEntity();
+        didEntity.setProperties(properties);
+        didEntity.setDid(did);
+
+        upload = JSON.toJSONString(didEntity);
     }
 
     public void showData(){
@@ -43,6 +74,7 @@ public class ElaDidDataTool {
         System.out.println("didPrivateKey: "+didPrivateKey);
         System.out.println("didPublicKey: "+didPublicKey);
         System.out.println("did: "+did);
+        System.out.println("upload data: "+upload);
     }
 
     public static void main(String[] args) throws Exception {
@@ -51,10 +83,42 @@ public class ElaDidDataTool {
 
         if (params.isEmpty()) {
             System.out.println("Should add app name in parameter");
+            return;
+        }
+
+        String appName = "";
+        String mnemonic = "";
+
+        for (int i = 0; i < params.size(); i++) {
+            String param = params.get(i);
+            if (param.equals("--h") || param.equals("--help")) {
+                System.out.println("Should add app name in parameter");
+            } else if (param.equals("--name")) {
+                i += 1;
+                appName = params.get(i);
+            } else if (param.equals("--mne")) {
+                i += 1;
+                int j = i;
+                for (; j < i + 11; j++) {
+                    mnemonic += params.get(j) + " ";
+                }
+                mnemonic += params.get(j);
+                i = j;
+            } else {
+                System.out.println("not support");
+                return;
+            }
+
+        }
+
+        if (appName.isEmpty()) {
+            System.out.println("Should add app name in parameter");
+            return;
         }
 
         ElaDidDataTool tool = new ElaDidDataTool();
-        tool.createAppInfoAndDid(params.get(0));
+        tool.createAppInfoAndDid(appName, mnemonic);
         tool.showData();
     }
+
 }
